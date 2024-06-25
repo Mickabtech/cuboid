ba148363a3006ed991b62ae50ac9cd907f8feeb9
Mickab <mickabtech@gmail.com>
2024-06-25 14:54:17 +0100

    updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,4 @@
-const UserModel = require('./../models/UserModel')
+const UserModel = require('../models/UserModel.js')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const validator = require('validator')
